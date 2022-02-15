8239941fa034d242ecf1668b499c97f1f82ecf32
jubaal <jubaal2122daw2@protonmail.com>
2022-02-15 13:06:49 +0100

    diseño vista principal. modificación elementos material, creación mangas dinamicamente

--- comics-angular/src/app/app.component.ts
+++ comics-angular/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'comics-angular';
+  tiles: any[] = [
+    {img: 'assets/Rengoku.png', name: 'Kimetsu no Yaiba'},
+    {img: 'assets/tokyorevengers.png', name: 'Tokyo Revengers'},
+    {img: 'assets/fullmetal.png', name: 'Full Metal Alchemist'},
+    {img: 'assets/erased.png', name: 'Erased'},
+    {img: 'assets/shingeki.png', name: 'Shingeki no Kyojin'},
+    {img: 'assets/jujutsu.png', name: 'Jujutsu Kaisen'},
+    {img: 'assets/bokunohero.png', name: 'Boku no Hero Academia'},
+    {img: 'assets/given.png', name: 'Given'},
+  ];
 }
