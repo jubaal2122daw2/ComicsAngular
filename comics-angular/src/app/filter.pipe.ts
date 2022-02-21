e05d25219349025e25b6e3e3fa99319377810463
jubaal <jubaal2122daw2@protonmail.com>
2022-02-21 09:31:46 +0100

    pipe funcional a medias

--- comics-angular/src/app/filter.pipe.ts
+++ comics-angular/src/app/filter.pipe.ts
@@ -11,7 +11,7 @@ export class FilterPipe implements PipeTransform {
   transform(array: Comic[], ...args: any): Comic[] {
 
     var matchGenre: boolean = false;
-    let arraySet = [...args[2]]
+    let arraySet:string[] = Array.from(args[2]);
 
       // comic.types.forEach(x => {
       //   if(args[2].has(x)){
@@ -21,24 +21,25 @@ export class FilterPipe implements PipeTransform {
       // if(args[2].size==0){
       //   matchGenre = true;
       // // }
-      // if(comic.name.toLowerCase().indexOf(args[0].toLowerCase()) > -1 
-      // && comic.autor.toLowerCase().indexOf(args[1].toLowerCase()) > -1 
+      // if(comic.name.toLowerCase().indexOf(args[0].toLowerCase()) > -1
+      // && comic.autor.toLowerCase().indexOf(args[1].toLowerCase()) > -1
       // && matchGenre){
       //   results.push(comic);
       // };
       if (array){
-        
-        
+
+
         const titolRegex = new RegExp(args[0],'ig');
         const autorRegex = new RegExp(args[1],'ig');
         const genreRegex = new RegExp(args[2],'ig');
         const results = array.filter((x) => x.name.match(titolRegex) && x.autor.match(autorRegex) && checkgenero(x.types));
         return results;
       }
-    
+
       return [];
 
       function checkgenero(generos:string[]):boolean{
+        if(arraySet.length==0) return true;
         for(let i:number=0 ; i < arraySet.length; i++){
           if(generos.includes(arraySet[i])){
             return true;
@@ -49,4 +50,3 @@ export class FilterPipe implements PipeTransform {
   }
 
 }
-
